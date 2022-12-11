6ee104e0e88353a2ac56d4045665a5c1f675bcc8
Andrea Ares <110069991+aresaurus@users.noreply.github.com>
2022-12-11 23:18:38 +0100

    Fixed an issue that prevented new inputs from showing correctly

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const balance = document.getElementById('balance-amount');
-const userIncome = document.getElementById('income');
-const userExpense = document.getElementById('expense');
-const concept = document.getElementById('add-concept');
-const amount = document.getElementById('add-amount');
-const history = document.getElementById('transaction-history');
-const form = document.getElementById('transaction-data');
+const balance = document.getElementById('balance-amount')
+const userIncome = document.getElementById('income')
+const userExpense = document.getElementById('expense')
+const concept = document.getElementById('add-concept')
+const amount = document.getElementById('add-amount')
+const history = document.getElementById('transaction-history')
+const form = document.getElementById('transaction-data')
 
-const localStorageTransactions = JSON.parse(
-    window.localStorage.getItem('transactions')
-);
-
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
 let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+
 
 function newTransaction(event) {
     event.preventDefault();
 
     const transaction = {
-        id: Math.floor(Math.random() * 100000),
         concept: concept.value,
-        amount: amount.value
+        amount: parseInt(amount.value),
+        id: Math.floor(Math.random() * 100000),
     }
 
     transactions.push(transaction)
@@ -32,30 +31,33 @@ function newTransaction(event) {
 }
 
 function addTransaction(transaction) {
-	const newItem = document.createElement('li');
-	newItem.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+    const newItem = document.createElement('li');
+    newItem.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     newItem.innerHTML = `
-    ${transaction.concept} <span>${Math.abs(transaction.amount)}</span> €
-    <button class="delete-btn" onclick="deleteTransaction(${transaction.id})">x</button>
+        ${transaction.concept} <span>${transaction.amount}</span>
+        <button class="delete-btn" onclick="deleteTransaction(${transaction.id})">x</button>
     `
-	history.appendChild(newItem)
+    history.appendChild(newItem)
 }
 
 function updateValues() {
-    const amounts = transactions.map(transaction => Number(transaction.amount));
 
+    const amounts = transactions.map(transaction => transaction.amount);
+    console.log(amounts)
+    const totalBalance = amounts.reduce((acc, item) => acc += item, 0).toFixed(2);
+    
     const totalIncome = amounts.filter(amount => amount > 0).reduce((acc, newIncome) => acc += newIncome, 0).toFixed(2);
 
-	const totalExpenses = amounts.filter(amount => amount < 0).reduce((acc, newExpense)=> ((acc += newExpense), 0) * -1).toFixed(2);
-
-	const totalBalance = amounts.reduce((acc, item)=> ((acc += item), 0)).toFixed(2);
-	
+    const totalExpenses = (amounts.filter(amount => amount < 0).reduce((acc, newExpense)=> acc += newExpense, 0) * -1).toFixed(2);
+    
 
     userIncome.innerText = `${totalIncome} €`;
     userExpense.innerText = `${totalExpenses} €`;
     balance.innerText = `${totalBalance} €`;
 }
+
 
 function deleteTransaction(id) {
     transactions = transactions.filter(transaction => transaction.id !== id);
@@ -65,15 +67,15 @@ function deleteTransaction(id) {
 }
 
 
-function updateLocalStorage() {
-    localStorage.setItem('transactions', JSON.stringify(transactions));
-}
-
 function init() {
     history.innerHTML = "";
     transactions.forEach(addTransaction);
     updateValues();
 }
 
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
 init();
-form.addEventListener('submit', addTransaction);
+form.addEventListener('submit', newTransaction);
